281ac6ce25f1c247245bcde717a18953c9983f57
Razvan <razvanz@users.noreply.github.com>
2018-05-24 16:15:38 +0200

    fix: prevent fail on missing auth scheme in header (#5)

--- test/token-extractors/auth-header-extractor-test.js
+++ test/token-extractors/auth-header-extractor-test.js
@@ -13,4 +13,14 @@ describe('AuthHeaderExtractor', () => {
 
     expect(bearerExtractor(req)).to.equal('helloworld')
   })
+
+  it('does not fail for missing scheme in Authorization header', () => {
+    const req = {
+      headers: {
+        authorization: 'helloworld'
+      }
+    }
+
+    expect(bearerExtractor.bind(bearerExtractor, req)).to.not.throw(TypeError)
+  })
 })
